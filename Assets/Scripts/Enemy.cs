11b899a191566aa206cb621018bf80942798b766
Turtle Bean <lincolndanger@gmail.com>
2022-11-08 16:54:09 -0800

    nav mesh is full

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform good;
+    NavMeshAgent agent;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+		agent.updateRotation = false;
+		agent.updateUpAxis = false;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = good.position;
     }
 
     void OnTriggerEnter2D(Collider2D collider)
